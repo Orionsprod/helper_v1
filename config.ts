3741b1adb270bcca69c3871f4a17b81f08088a00
Orionsprod <138870673+Orionsprod@users.noreply.github.com>
2025-05-03 20:09:22 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const NOTION_TOKEN = Deno.env.get("NOTION_TOKEN")!;
-export const GOOGLE_DRIVE_API_KEY = Deno.env.get("GOOGLE_DRIVE_API_KEY")!;
 export const GOOGLE_ROOT_FOLDER_ID = Deno.env.get("GOOGLE_ROOT_FOLDER_ID")!;
+export const SERVICE_ACCOUNT_JSON = Deno.env.get("GOOGLE_SERVICE_ACCOUNT_JSON")!;
 export const DEBUG = Deno.env.get("DEBUG") === "true";
